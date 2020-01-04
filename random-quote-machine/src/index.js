7c7464d072058e9e8d477432973c35268fc89249
Li Xuetao <lixuetaoleo@163.com>
2020-01-04 19:40:05 +0800

    添加了root下的几个组件

--- random-quote-machine/src/index.js
+++ random-quote-machine/src/index.js
@@ -1,12 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const quotes = [
+    {
+        author: "Test",
+        quote: "tttt"
+    },
+    {
+        author: "Test1",
+        quote: "tttt1"
+    },
+    {
+        author: "Test2",
+        quote: "tttt2"
+    },
+    {
+        author: "Test3",
+        quote: "tttt3"
+    },
+    {
+        author: "Test4",
+        quote: "tttt4"
+    },
+    {
+        author: "Test5",
+        quote: "tttt5"
+    },
+    {
+        author: "Test6",
+        quote: "tttt6"
+    },
+    {
+        author: "Test7",
+        quote: "tttt7"
+    },
+    {
+        author: "Test8",
+        quote: "tttt8"
+    },
+    {
+        author: "Test9",
+        quote: "tttt9"
+    },
+    {
+        author: "Test10",
+        quote: "tttt10"
+    }
+];
+class QuoteWords extends React.Component{
+    render(){
+        return(
+        <div id={this.props.id}>{quotes[this.props.randomnum].author}</div>
+        );
+    }
+}
+class Author extends React.Component{
+    render(){
+        return(
+        <div id={this.props.id}>{quotes[this.props.randomnum].quote}</div>
+        );
+    }
+}
+class App extends React.Component{
+    render() {
+        const random = Math.floor(Math.random() * 10);
+        return (
+            <div id="quote-box">
+                <QuoteWords id="text" randomnum={random}/>
+                <Author id="author" randomnum={random}/>
+            </div>       
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
