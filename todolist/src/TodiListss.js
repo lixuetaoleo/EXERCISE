22d31a4843a5c01d01b8711565517f09f7ee3e26
Li Xuetao <lixuetaoleo@163.com>
2020-01-12 21:13:45 +0800

    TodoListss包含了关于react-redux的用法

---
+++ todolist/src/TodiListss.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+
+class TodoListss extends Component{
+
+};
+
+export default TodoListss
--- todolist/src/index.js
+++ todolist/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import TodoList from './TodoList';
 // import App from './App'
-import TodoLists from './TodoLists';
+// import TodoLists from './TodoLists';
+import TodoListss from './TodoListss'
 
 // ReactDOM.render(<TodoList />, document.getElementById('root'));
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<TodoLists />, document.getElementById('root'));
+// ReactDOM.render(<TodoLists />, document.getElementById('root'));
 
+
+ReactDOM.render(<TodoListss />, document.getElementById('root'));
